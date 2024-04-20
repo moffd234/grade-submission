8bdb0ef96dcc9aa6ada5b497b0786aec38c842d5
moffd234 <moffd234@gmail.com>
2024-04-20 19:10:13 -0400

    Made grade class

--- src/main/java/com/ltp/gradesubmission/GradesController.java
+++ src/main/java/com/ltp/gradesubmission/GradesController.java
@@ -1,12 +1,14 @@
 package com.ltp.gradesubmission;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GradesController {
 
     @GetMapping("/grades")
-    public String getGrades(){
+    public String getGrades(Model model){
+
         return "grades"; // Directs the grades.html
     }
 }
