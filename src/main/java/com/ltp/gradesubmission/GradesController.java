69c6ad7bcd98b348e029e5e2ec3d0a3ed1969505
moffd234 <moffd234@gmail.com>
2024-04-27 17:48:11 -0400

    Switched to external style sheet

--- src/main/java/com/ltp/gradesubmission/GradesController.java
+++ src/main/java/com/ltp/gradesubmission/GradesController.java
@@ -3,13 +3,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class GradesController {
+    List<Grade> studentGrades = new ArrayList<>();
 
     @GetMapping("/grades")
     public String getGrades(Model model){
-        Grade grade = new Grade("Harry", "Potions", "C-");
-        model.addAttribute("grade", grade);
+        studentGrades.add(new Grade("Harry", "Potions", "C-"));
+        studentGrades.add(new Grade("Hermione", "DADA", "A+"));
+        studentGrades.add(new Grade("Neville", "Charms", "A-"));
+
+        model.addAttribute("grades", studentGrades);
         return "grades"; // Directs the grades.html
     }
 }
