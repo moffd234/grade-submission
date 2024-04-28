acf595a661871196c8dda685a39c00cbdd93006e
moffd234 <moffd234@gmail.com>
2024-04-28 15:02:56 -0400

    Added ability to display user inputted grades

--- src/main/java/com/ltp/gradesubmission/GradesController.java
+++ src/main/java/com/ltp/gradesubmission/GradesController.java
@@ -2,6 +2,7 @@ package com.ltp.gradesubmission;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,11 +10,8 @@ import java.util.List;
 
 @Controller
 public class GradesController {
-    List<Grade> studentGrades = Arrays.asList(
-            new Grade("Harry", "Potions", "C-"),
-            new Grade("Hermione", "DADA", "A+"),
-            new Grade("Neville", "Charms", "A-")
-    );
+    List<Grade> studentGrades = new ArrayList<>();
+
  
     @GetMapping("/grades")
     public String getGrades(Model model){
@@ -25,5 +23,11 @@ public class GradesController {
     public String gradeForm(Model model){
         model.addAttribute("grade", new Grade());
         return "form";
+    }
+
+    @PostMapping("/handleSubmit")
+    public String handleSubmit(Grade grade){
+        studentGrades.add(grade); // Adds the grade that was submitted in the POST request
+        return "redirect:/grades"; // Redirects to a GET request at /grades
     }
 }
